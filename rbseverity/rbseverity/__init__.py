3f571d901b6e43607b412603281780b6058e8b1b
Christian Hammond <chipx86@chipx86.com>
2014-07-19 02:05:45 -0700

    [rbseverity] Release 0.5.1.

--- rbseverity/rbseverity/__init__.py
+++ rbseverity/rbseverity/__init__.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 5, 0, 0, 'final', 0, True)
+VERSION = (0, 5, 1, 0, 'final', 0, True)
 
 
 def get_version_string():

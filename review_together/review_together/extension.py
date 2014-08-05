b1a86e61553056d2bf432ce7febf0695838a8e22
Peter Tran <pete.tran02@gmail.com>
2014-08-05 14:29:35 -0700

    Overrode TogetherJS default css to make the extension look more like Review Board.
    This change adds some CSS to make the TogetherJS UI fit in better with Review
    Board's color scheme and design.
    
    Testing Done:
    Launch 'Chat' by clicking the Chat tab located on the right side of the header
    bar. The following views on any page illustrate the new changes:
    
    - The right TogetherJS menu should be dark and similar to the header bar.
    - The pop-up titled "Invite a friend" should mimic containers in the admin
      panel.
    - Clicking on the first icon in the TogetherJS menu, the secondary menu that
      pops up mimics the pull down shown the Support tab in the header bar is
      hovered over.
    - Clicking the last icon (Chat) or third icon (Audio Chat) in the TogetherJS
      menu, the pop-up should also mimic the containers found in the admin panel.
    
    Reviewed at https://reviews.reviewboard.org/r/6066/

--- review_together/review_together/extension.py
+++ review_together/review_together/extension.py
@@ -1,17 +1,22 @@
 # reviewboard-together Extension for Review Board.
-from django.conf import settings
-from django.conf.urls import patterns, include
 from reviewboard.extensions.base import Extension
-from reviewboard.extensions.hooks import HeaderActionHook, HeaderDropdownActionHook, TemplateHook
+from reviewboard.extensions.hooks import HeaderActionHook, TemplateHook
 
 class ReviewTogether(Extension):
+    # This adds reviewboard specific styling of the TogetherJS extension.
+    css_bundles = {
+        'default': {
+            'source_filenames': ['css/review-together.less'],
+        },
+    }
+
     def __init__(self, *args, **kwargs):
         super(ReviewTogether, self).__init__(*args, **kwargs)
-        self.script_injection = TemplateHook(self, "base-scripts-post",
+        self.script_injection = TemplateHook(
+            self, "base-scripts-post",
             template_name="review_together/base.html")
         self.button = HeaderActionHook(self, [{
-          "id": "launch-together",
-          "label": "Chat",
-          "url": "#"
+            "id": "launch-together",
+            "label": "Chat",
+            "url": "#"
         }]);
-

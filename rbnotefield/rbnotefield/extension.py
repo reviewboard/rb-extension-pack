e41239d31a1c1f4e819d733618a87ff47459503f
Christian Hammond <christian@beanbaginc.com>
2014-09-03 12:36:51 -0700

    [rbnotefield] Work around a breakage with rendering change entries.
    Review Board < 2.0.7 breaks when trying to render changes to the Note
    field if adding content for the first time after the review request has
    already been published. The old value for the field was 'None', which
    broke the rendering due to the expectation of strings.
    
    While Review Board 2.0.7 is getting a fix for this, we still want to
    work around it for earlier releases. This adds a new
    render_change_entry_html() function that just normalizes the values
    before calling the parent function, ensuring we're dealing with empty
    strings.
    
    Fixes bug #3549
    Reviewed at https://reviews.reviewboard.org/r/6291/

--- rbnotefield/rbnotefield/extension.py
+++ rbnotefield/rbnotefield/extension.py
@@ -9,6 +9,17 @@ class NoteField(BaseTextAreaField):
     label = _('Note to Reviewers')
     enable_markdown = True
 
+    def render_change_entry_html(self, info):
+        # Work around a bug in Review Board < 2.0.7, where a 'None' value
+        # would break change rendering.
+        if 'old' in info and info['old'][0] is None:
+            info['old'] = ['']
+
+        if 'new' in info and info['new'][0] is None:
+            info['new'] = ['']
+
+        return super(NoteField, self).render_change_entry_html(info)
+
 
 class NoteFieldExtension(Extension):
     metadata = {

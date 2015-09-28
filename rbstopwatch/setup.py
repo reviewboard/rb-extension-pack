0c84df60ea6668790ea1ef600357343ce99b7d86
David Trowbridge <trowbrds@gmail.com>
2015-09-28 16:47:16 -0700

    Make some additional tweaks to the rbstopwatch setup.py
    This tweaks the package metadata a bit, and makes the extension entry points a
    list (since bare strings are deprecated).
    
    Testing done:
    Built the rbstopwatch package.
    
    Reviewed at https://reviews.reviewboard.org/r/7665/

--- rbstopwatch/setup.py
+++ rbstopwatch/setup.py
@@ -5,23 +5,35 @@ from reviewboard.extensions.packaging import setup
 from rbstopwatch import get_package_version
 
 
-PACKAGE = "rbstopwatch"
-
-
 setup(
-    name=PACKAGE,
+    name='rbstopwatch',
     version=get_package_version(),
     description='Stopwatch extension for Review Board',
-    author='Beanbag, Inc. <support@beanbaginc.com>',
+    url='https://www.reviewboard.org/',
+    author='Beanbag, Inc.',
+    author_email='support@beanbaginc.com',
+    maintainer='Beanbag, Inc.',
+    maintainer_email='support@beanbaginc.com',
     packages=['rbstopwatch'],
     entry_points={
-        'reviewboard.extensions':
-            '%s = rbstopwatch.extension:StopwatchExtension' % PACKAGE,
+        'reviewboard.extensions': [
+            'rbstopwatch = rbstopwatch.extension:StopwatchExtension',
+        ],
     },
     package_data={
         b'rbstopwatch': [
             'templates/rbstopwatch/*.txt',
             'templates/rbstopwatch/*.html',
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Review Board',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
 )

551b2705919fe58eb67cffb9c64abcb7f8635fbe
David Trowbridge <trowbrds@gmail.com>
2015-09-29 18:25:22 -0700

    Update setup.py and versioning for rbcommenttype.
    This brings the setup for the comment type extension into line with our
    standard practices.
    
    Testing done:
    Tested this same setup with the stopwatch extension.
    
    Reviewed at https://reviews.reviewboard.org/r/7666/

--- rbcommenttype/setup.py
+++ rbcommenttype/setup.py
@@ -2,27 +2,38 @@ from __future__ import unicode_literals
 
 from reviewboard.extensions.packaging import setup
 
+from rbcommenttype import get_package_version
 
-PACKAGE = 'rbcommenttype'
-VERSION = '0.1'
 
 setup(
-    name=PACKAGE,
-    version=VERSION,
+    name='rbcommenttype',
+    version=get_package_version(),
     description='Comment type categorization for Review Board',
+    url='https://www.reviewboard.org/',
     author='Beanbag, Inc.',
     author_email='support@beanbaginc.com',
     maintainer='Beanbag, Inc.',
     maintainer_email='support@beanbaginc.com',
-    packages=['rbcommenttype'],
+    packages=[b'rbcommenttype'],
     entry_points={
-        'reviewboard.extensions':
-            '%s = rbcommenttype.extension:CommentTypeExtension' % PACKAGE,
+        'reviewboard.extensions': [
+            'rbcommenttype = rbcommenttype.extension:CommentTypeExtension',
+        ]
     },
     package_data={
         b'rbcommenttype': [
             'templates/rbcommenttype/*.html',
             'templates/rbcommenttype/*.txt',
         ],
-    }
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Review Board',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
 )

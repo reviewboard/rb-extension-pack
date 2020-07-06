4ab78f48a08caa569fc90bbc79d9ecf3fafdc8b6
David Trowbridge <trowbrds@gmail.com>
2020-07-06 16:12:21 -0600

    [rbnotefield] Modernize rbnotefield.
    This change modernizes the code for the "Note to Reviewers" extension.
    This extension is super duper tiny, so the change is minimal.
    
    Testing Done:
    Smoke tested the extension.
    
    Reviewed at https://reviews.reviewboard.org/r/11073/

--- rbnotefield/rbnotefield/extension.py
+++ rbnotefield/rbnotefield/extension.py
@@ -1,3 +1,7 @@
+"""Review Board Extension for adding a "notes" field."""
+
+from __future__ import unicode_literals
+
 from django.utils.translation import ugettext_lazy as _
 from reviewboard.extensions.base import Extension
 from reviewboard.extensions.hooks import ReviewRequestFieldsHook
@@ -5,11 +9,23 @@ from reviewboard.reviews.fields import BaseTextAreaField
 
 
 class NoteField(BaseTextAreaField):
+    """The 'Note to Reviewers' field."""
+
     field_id = 'beanbag_notefield_notes'
     label = _('Note to Reviewers')
     enable_markdown = True
 
     def render_change_entry_html(self, info):
+        """Render the data for the change description.
+
+        Args:
+            info (dict):
+                The info dict from the ChangeDescription object.
+
+        Returns:
+            django.utils.safestring.SafeText:
+            The rendered change entry HTML.
+        """
         # Work around a bug in Review Board < 2.0.7, where a 'None' value
         # would break change rendering.
         if 'old' in info and info['old'][0] is None:
@@ -22,6 +38,8 @@ class NoteField(BaseTextAreaField):
 
 
 class NoteFieldExtension(Extension):
+    """The 'Note to Reviewers' extension."""
+
     metadata = {
         'Name': _('Note to Reviewers'),
         'Summary': _('Add a "Note to Reviewers" field to review requests, '
@@ -30,4 +48,5 @@ class NoteFieldExtension(Extension):
     }
 
     def initialize(self):
+        """Initialize the extension."""
         ReviewRequestFieldsHook(self, 'main', [NoteField])

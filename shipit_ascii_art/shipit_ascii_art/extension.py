f24d47aadaac567431871a3776928d10371e1673
David Trowbridge <trowbrds@gmail.com>
2020-07-20 17:12:03 -0600

    [shipit-ascii-art] Modernize the ASCII Art extension.
    This change modernizes the Ship It! ASCII Art extension and dramatically
    simplifies the codebase by consolidating everything into the extension
    file, while deleting unused code. Most notably, it switches to using
    SignalHook to connect the signal handler, which will behave correctly
    when deactivating the extension.
    
    Testing Done:
    Activated the extension and verified the results.
    
    Reviewed at https://reviews.reviewboard.org/r/11088/

--- shipit_ascii_art/shipit_ascii_art/extension.py
+++ shipit_ascii_art/shipit_ascii_art/extension.py
@@ -1,15 +1,98 @@
-# shipit_ascii_art Extension for Review Board.
-from reviewboard.extensions.base import Extension
+"""Ship It! ASCII Art extension for Review Board."""
 
-from shipit_ascii_art.handlers import SignalHandlers
+from __future__ import unicode_literals
+
+from reviewboard.extensions.base import Extension
+from reviewboard.extensions.hooks import SignalHook
+from reviewboard.reviews.signals import review_published
+
+
+art_basic = """
+      _~
+   _~)_)_~
+  )_))_))_)
+  _!__!__!_
+  \______t/
+~~~~~~~~~~~~~
+"""
+
+art_juggernaut_small = """
+    __4___
+ _  \ \ \ \
+<'\ /_/_/_/
+ ((____!___/)
+  \\0\\0\\0\\0\/
+ ~~~~~~~~~~~
+"""
+
+art_juggernaut_big = """
+                       _____|\
+                  _.--| R B |:
+                 <____|.----||
+                        .---''---,
+                         ;..__..'    _...
+                       ,'/  ;|/..--''    \
+                     ,'_/.-/':            :
+                _..-'''/  /  |  \    \   _|/|
+               \      /-./_ \;   \    \,;'   \
+               ,\    / \:  `:\    \   //    `:`.
+             ,'  \  /-._;   | :    : ::    ,.   .
+           ,'     ::   /`-._| |    | || ' :  `.`.)
+        _,'       |;._:: |  | |    | `|   :    `'
+      ,'   `.     /   |`-:_ ; |    |  |  : \
+      `--.   )   /|-._:    :          |   \ \
+         /  /   :_|   ;`-._;   __..--';    : :
+        /  (    ;|;-./_  _/.-:'o |   /     ' |
+       /  , \._/_/_./--''/_|:|___|_,'        |
+      :  /   `'-'--'----'---------'          |
+      | :     O ._O   O_. O ._O   O_.      ; ;
+      : `.      //    //    //    //     ,' /
+    ~~~`.______//____//____//____//_______,'~
+              //    //~   //    //
+       ~~   _//   _//   _// ~ _//     ~
+     ~     / /   / /   / /   / /  ~      ~~
+          ~~~   ~~~   ~~~   ~~~
+"""
+
+art = {
+    'basic': art_basic,
+    'juggernaut_small': art_juggernaut_small,
+    'juggernaut_big': art_juggernaut_big,
+}
 
 
 class AsciiArt(Extension):
-    is_configurable = True
+    """Ship It! ASCII Art extension for Review Board."""
 
     def __init__(self, *args, **kwargs):
         super(AsciiArt, self).__init__(*args, **kwargs)
-        self.signal_handlers = SignalHandlers(self)
-        # This variable will be changed or refactored when
-        # Ascii art is made configurable
-        self.ascii_pattern = "basic"
+
+        self.ascii_pattern = 'basic'
+
+        SignalHook(self, review_published, self._on_review_published)
+
+    def _on_review_published(self, *args, **kwargs):
+        """Handler for when a review is published.
+
+        Args:
+            *args (tuple):
+                Positional arguments passed through the signal.
+
+            **kwargs (dict):
+                Keyword arguments passed through the signal.
+        """
+        review = kwargs.get('review')
+
+        # Only add the ship-it ASCII art if this review has a ship-it.
+        if review.ship_it:
+            rich = hasattr(review, 'rich_text') and review.rich_text
+
+            if rich:
+                review.body_top += '\n```\n'
+
+            review.body_top += art[self.ascii_pattern]
+
+            if rich:
+                review.body_top += '```\n'
+
+            review.save()
--- shipit_ascii_art/shipit_ascii_art/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template.context import RequestContext
-
-
-def configure(request, template_name="shipit_ascii_art/configure.html"):
-    return render_to_response(template_name, RequestContext(request))

13d5ca36c0aa9c50574bd5ac5da139117fa6a1ee
Christian Hammond <christian@beanbaginc.com>
2022-05-10 16:41:49 -0700

    [rbseverity] Release 0.6 for Python 2 and 3.

--- rbseverity/rbseverity/__init__.py
+++ rbseverity/rbseverity/__init__.py
@@ -9,7 +9,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, Patch, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 5, 1, 0, 'final', 0, True)
+VERSION = (0, 6, 0, 0, 'final', 0, True)
 
 
 def get_version_string():
